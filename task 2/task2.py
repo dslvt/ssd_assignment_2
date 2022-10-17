0b3e8d3376fdc41fcf5d8eff17c07eaf7d4444b8
Salavat Dinmukhametov <perfog@mail.ru>
2022-10-17 21:57:13 +0300

    add first cmd

--- task 2/task2.py
+++ task 2/task2.py
@@ -1,7 +1,38 @@
+from urls import file_paths, file_names
+from os import listdir
+from os.path import isfile, join
+import pandas as pd
+
 data = None
+DATA_PATH = "data/"
+
+
+def timestamp_calc(df):
+    start_timestamp = df["timestamp"].min()
+    end_timestamp = df["timestamp"].max()
+    return end_timestamp - start_timestamp
+
 
 def cmd_get_status():
-    pass
+    current_date = data["timestamp"].max()
+    cut_off = current_date - pd.offsets.Day(7)
+    filtered_df = data[data["timestamp"] > cut_off]
+
+    total_sesstions = filtered_df["session_id"].unique().shape[0]
+
+    timestamps = filtered_df.groupby(by=["client_user_id", "session_id"]).apply(
+        timestamp_calc
+    )
+    timestamps = timestamps.to_frame()
+
+    avg_time = timestamps[0].mean()
+    hours_spend = timestamps[0].sum()
+
+    s = "Statistics for the past 7 days:\n"
+    s += f"\tTotal sessions : {total_sesstions}\n"
+    s += f"\tAverage time spent per session : {avg_time}\n"
+    s += f"\tSum of hours spent by all users : {hours_spend}\n"
+    print(s)
 
 
 def cmd_print_summary():
@@ -13,7 +44,16 @@ def cmd_predict_next_session():
 
 
 def cmd_fetch_new_data():
-    pass
+    downloaded_files = get_file_names(DATA_PATH)
+    idx = len(downloaded_files)
+
+    if idx < 30:
+        new_frame = download_file(file_paths[idx], file_names[idx])
+        new_frame["timestamp"] = pd.to_datetime(new_frame["timestamp"])
+
+        return new_frame
+    else:
+        print("All files already downloaded!")
 
 
 def cmd_get_top_users():
@@ -24,7 +64,30 @@ def cmd_exit():
     pass
 
 
+def get_file_names(directory):
+    return [f for f in listdir(directory) if isfile(join(directory, f))]
+
+
+def download_file(path, file_name):
+    url = "https://drive.google.com/uc?id="
+    url_name = url + path.split("/")[-2]
+    df = pd.read_csv(url_name)
+    df.to_csv(DATA_PATH + file_name)
+    return df
+
+
 if __name__ == "__main__":
+    downloaded_file_paths = get_file_names(DATA_PATH)
+    print(downloaded_file_paths)
+    if len(downloaded_file_paths) == 0:
+        data = download_file(file_paths[0], file_names[0])
+    else:
+        downloaded_files = []
+        for file_path in downloaded_file_paths:
+            downloaded_files.append(pd.read_csv(DATA_PATH + file_path))
+        data = pd.concat(downloaded_files, ignore_index=True)
+    data["timestamp"] = pd.to_datetime(data["timestamp"])
+
     while True:
         print(
             """Choose one operation from below :
@@ -45,10 +108,12 @@ if __name__ == "__main__":
         elif input_number == 3:
             cmd_predict_next_session()
         elif input_number == 4:
-            cmd_fetch_new_data()
+            new_frame = cmd_fetch_new_data()
+            data = pd.concat([data, new_frame])
         elif input_number == 5:
             cmd_get_top_users()
         elif input_number == 6:
+            cmd_get_status()
             cmd_exit()
             break
         else:
---
+++ task 2/urls.py
@@ -0,0 +1,34 @@
+file_paths = [
+    "https://drive.google.com/file/d/14T7-4UtDj-azvs3s0U_I2FVBsKVXLbaK/view?usp=sharing",  # 1
+    "https://drive.google.com/file/d/1GdUmvOH0eZu8bBYujTzpWuyCF9KHwnB7/view?usp=sharing",  # 2
+    "https://drive.google.com/file/d/10mv4oV2WX1CajgOvOwtiIWmsrVc5NDGB/view?usp=sharing",  # 3
+    "https://drive.google.com/file/d/1CaFw_CQ9FuDOCWjKjBRkMH337qS69rpu/view?usp=sharing",  # 4
+    "https://drive.google.com/file/d/12MDQVHHf5rCEqZmF4svqbnI4fJh6Lc5o/view?usp=sharing",  # 5
+    "https://drive.google.com/file/d/1e9swY5cQ24-8SVyOIq-5tJiZlYA-mGLo/view?usp=sharing",  # 6
+    "https://drive.google.com/file/d/19_X0XetkAQYmysb6J8bCyqTXhJhSobl8/view?usp=sharing",  # 7
+    "https://drive.google.com/file/d/1qGIM8HWW3Uwf8-mEXQNtmNOXc6NsMDor/view?usp=sharing",  # 8
+    "https://drive.google.com/file/d/1mBGjKz8nLPoNXTqDzbTpkX8NTdIknpnL/view?usp=sharing",  # 9
+    "https://drive.google.com/file/d/1y9Y8K9NP--JBxs__j8NtK7fZQZvFoZbp/view?usp=sharing",  # 10
+    "https://drive.google.com/file/d/19RdAzgjhYDy0R6KYrVfd-ufbiGxhbdAc/view?usp=sharing",  # 11
+    "https://drive.google.com/file/d/1dObmHddKXE6HPNsYfceRB3kT2WnIH8Hb/view?usp=sharing",  # 12
+    "https://drive.google.com/file/d/1bI3xK_130tyTArn-0aD4DCkFsI80Y8-a/view?usp=sharing",  # 13
+    "https://drive.google.com/file/d/15ELVHoWXKpk1E5kTJ0pOH29MUGrGV5aD/view?usp=sharing",  # 14
+    "https://drive.google.com/file/d/1oDd99rZ7cU3OBcY8mB3FzRXrbyS6snbg/view?usp=sharing",  # 16
+    "https://drive.google.com/file/d/1luCiuS5nrjHmnlfWxUhN8M7iPXY5UfzY/view?usp=sharing",  # 16
+    "https://drive.google.com/file/d/1EClb7YyDsxLQa2FOEJ0MZR7D3avSRPX4/view?usp=sharing",  # 17
+    "https://drive.google.com/file/d/1q3gfL9pLrGW48gNWPrYc7SbVSqPAyBAR/view?usp=sharing",  # 18
+    "https://drive.google.com/file/d/12nGHrs5UA-xHU8W2fJferPs11-KhStNt/view?usp=sharing",  # 19
+    "https://drive.google.com/file/d/13n6N9I6KQJEkkFy9hlMSwYVPhyaoguHj/view?usp=sharing",  # 20
+    "https://drive.google.com/file/d/16BVDp4j7ocoF2Ft3OkVxM2YFEwr8MSdm/view?usp=sharing",  # 21
+    "https://drive.google.com/file/d/1daRgyVRlxXnpgcP5epqjskOF8kmQELr_/view?usp=sharing",  # 22
+    "https://drive.google.com/file/d/123YY-NUKMNLFPRdQZ8P8tdqhfkvBGHqj/view?usp=sharing",  # 23
+    "https://drive.google.com/file/d/1kEpet_EQa3pxZhzv8o5mMYRsh-W74ymh/view?usp=sharing",  # 24
+    "https://drive.google.com/file/d/14ln7QYQbAAzS4OGsb2xuckF7ZzPZJ-Wt/view?usp=sharing",  # 25
+    "https://drive.google.com/file/d/1_ENg5yu9tMS696uu89FePeJ0n-UV7JpN/view?usp=sharing",  # 26
+    "https://drive.google.com/file/d/1I5U-89IJCkRKojD2cmXBEARQtE4GjK8Z/view?usp=sharing",  # 27
+    "https://drive.google.com/file/d/1xyLVct6gSKbFDc_S-XqXKUv6GmCdILw6/view?usp=sharing",  # 28
+    "https://drive.google.com/file/d/1GozNfiERhZ3GL-LS3rzJqa0m1mjb4pKa/view?usp=sharing",  # 29
+    "https://drive.google.com/file/d/1BrDehL4YKbai48kkdsgD-ZDODFFzdLac/view?usp=sharing",
+]
+
+file_names = [f'raw_2022_09_{i:02}.csv' for i in range(1, 30)]
